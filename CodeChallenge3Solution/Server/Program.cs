f2751de9343f8b01cf0001105cdd58b4c918e248
Sarah Wood <sarah.wood@asos.com>
2018-02-13 16:41:31 +0000

    initial push

---
+++ CodeChallenge3Solution/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.Title = "Samples.StepByStep.Server";
+            var endpointConfiguration = new EndpointConfiguration("Samples.StepByStep.Server");
+            endpointConfiguration.UseSerialization<XmlSerializer>();
+            endpointConfiguration.EnableInstallers();
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.SendFailedMessagesTo("error");
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            try
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+            finally
+            {
+                await endpointInstance.Stop()
+                    .ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ CodeChallenge3Solution/Subscriber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.Title = "Samples.StepByStep.Subscriber";
+            var endpointConfiguration = new EndpointConfiguration("Samples.StepByStep.Subscriber");
+            endpointConfiguration.UseSerialization<XmlSerializer>();
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            try
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+            finally
+            {
+                await endpointInstance.Stop()
+                    .ConfigureAwait(false);
+            }
+        }
+    }
+}
